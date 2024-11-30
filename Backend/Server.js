1ffb5fae3b608a266adecfc23ec43150e9d547d4
Arvindsingh  Deora <114655155+Arvindsingh-Deora@users.noreply.github.com>
2024-11-30 10:02:47 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -35,7 +35,12 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Server is running and responding on the root route!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
